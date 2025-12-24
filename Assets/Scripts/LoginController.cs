bca0da5940cf94e178eb6a6751b76e7e460fa22c
Shirco3 <160611991+Shirco3@users.noreply.github.com>
2025-12-24 21:40:52 +0200

    Upload Unity project changes

---
+++ Assets/Scripts/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // פונקציה שתופעל כשילחצו על כפתור ה-Login
+    public void OnLoginButtonClicked()
+    {
+        // השם "Main" חייב להיות בדיוק כמו שם הסצנה השנייה שלך
+        SceneManager.LoadScene("Main");
+    }
+}
